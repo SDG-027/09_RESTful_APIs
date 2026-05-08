832439a4729dc532c05bc7b1528f79d8976ec1d8
Stephan Ullmann <stephan.ullmann@posteo.de>
2026-05-08 09:47:49 +0200

    file upload: starter

---
+++ 08_file-upload-formidable/backend/src/schemas/user.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod/v4';
+
+const userSchema = z.strictObject({
+  firstName: z.string().min(1, 'First name is required'),
+  lastName: z.string().min(1, 'Last name is required'),
+  email: z.email('Invalid email.'),
+  image: z
+    .url({
+      protocol: /^https?$/,
+      hostname: z.regexes.domain
+    })
+    .default(
+      'https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg'
+    )
+});
+
+export { userSchema };
